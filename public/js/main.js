67216748f9fdd37429a314b1f3dc699a026bd35a
Joshua Potter <tljoshh@gmail.com>
2018-11-06 00:23:46 -0500

    adding money and formatting updates

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ class Player {
       this.playerPoints = 0;
       this.playerLevel = 1;
       this.playerClickRate = 1;
+      this.playerMoney = 0;  
       this.playerAutoClickStatus = false;
       this.playerAutoClick;
    }
@@ -39,16 +40,23 @@ class Player {
    set clickRate(newClickRate) {
       this.playerClickRate = newClickRate;
    }
+   get money() {
+      return this.playerMoney;
+   }
+   set money(newMoney) {
+      this.playerMoney = newMoney;
+   }
    get autoClick() {
       return this.playerAutoClickStatus;
    }
    set autoClick(newAutoClick) {
       if(newAutoClick == true && this.playerAutoClickStatus == false) {
          $('.autoclick').html('AutoCode (✔)');
-         let t = this;
-         t.playerAutoClick = setInterval(function() {
-            t.addPoint();
-            appendMessage(t, "Lines of code: <strong>" + t.points + "</strong>");
+         let p = this;
+         p.playerAutoClick = setInterval(function() {
+            p.addPoint();
+            printLines(p);
+            updateMoney(p);
          }, 250);
       } else if (newAutoClick == false) {
          $('.autoclick').html('AutoCode');
@@ -58,6 +66,7 @@ class Player {
    }
    addPoint() {
       this.points += this.clickRate;
+      this.money += this.clickRate/100;
 
       // level = constant * sqrt(xp)
       let newLevel = .42/2 * Math.sqrt(this.points);
@@ -75,8 +84,8 @@ class Player {
    }
    saveToLocal() {
       // convert player object to string and save to localStorage
+      // optimize: only save if there are changes
       localStorage.setItem("player", JSON.stringify(this));
-      console.log(localStorage.getItem("player"));
       console.log("Session saved.");
    }
    loadFromLocal() {
@@ -90,6 +99,7 @@ class Player {
          this.points = p.playerPoints;
          this.level = p.playerLevel;
          this.clickRate = p.playerClickRate;
+         this.money = p.playerMoney;
          this.autoClick = p.playerAutoClickStatus;
          return true;
       } else {
@@ -106,12 +116,14 @@ class Player {
 $(document).ready(function() {
    // Main
    let p = new Player();
-   init(p, "Hello, World!");
+   let autoSave;
+   init(p, autoSave, "Hello, World!");
 
    // Events
    $('.click-region').click(function() {
       p.addPoint();
-      appendMessage(p, "Lines of code: <strong>" + p.points + "</strong>");
+      printLines(p);
+      updateMoney(p);
    });
 
    $('.new').click(function(e) {
@@ -133,22 +145,22 @@ $(document).ready(function() {
       appendMessage(p, "<span class='green'>Game saved.</span>");
    });
 
-   $('#export').click(function(e) {
-      let exportSave = document.createElement('a');
-      exportSave.style.visibility = "hidden";
-      exportSave.setAttribute('href', p.exportSave());
-      exportSave.setAttribute('download', 'code-clicker-save.json');
-      document.body.appendChild(exportSave);
-      exportSave.click();
-      appendMessage(p, "<span class='green'>Downloading save file.</span>");
-      document.body.removeChild(exportSave);
-   });
+   // $('#export').click(function(e) {
+   //    let exportSave = document.createElement('a');
+   //    exportSave.style.visibility = "hidden";
+   //    exportSave.setAttribute('href', p.exportSave());
+   //    exportSave.setAttribute('download', 'code-clicker-save.json');
+   //    document.body.appendChild(exportSave);
+   //    exportSave.click();
+   //    appendMessage(p, "<span class='green'>Downloading save file.</span>");
+   //    document.body.removeChild(exportSave);
+   // });
 
-   $('.clear').click(function(e) {
-      e.preventDefault();
-      clearMessages(p);
-      appendMessage(p, "<span class='green'>Window cleared.</span>");
-   });
+   // $('.clear').click(function(e) {
+   //    e.preventDefault();
+   //    clearMessages(p);
+   //    appendMessage(p, "<span class='green'>Window cleared.</span>");
+   // });
    
    $('.clickrate').click(function(e) {
       e.preventDefault();
@@ -168,13 +180,23 @@ $(document).ready(function() {
 
 });
 
-function init(p, welcome) {
-   console.log("Running...");
+function init(p, autoSave, welcome) {
+   // load previous save if found
    if(p.loadFromLocal()) {
       appendMessage(p, "Welcome back, " + p.adjective + p.noun + ".");
    } else {
       appendMessage(p, welcome);
    }
+
+   // set money
+   updateMoney(p);
+
+   // initialize autosave and run every 30 sec
+   autoSave = setInterval(function() {
+      p.saveToLocal();
+   }, 30000);
+
+   console.log("Running...");
    appendMessage(p, "Click to start programming...");
 }
 
@@ -186,10 +208,18 @@ function appendMessage(p, message, window = $('#messages')) {
          parentNode.removeChild(parentNode.childNodes[0]);
       }
    }
-   window.append("<p><!--<span class='output'>&gt;&gt;</span> --><span class='yellow'>$" + p.adjective + p.noun + "</span> <span class='output'>>></span> " + message + "</p>");
+   window.append("<p><span class='yellow'>$" + p.adjective + p.noun + "</span> <span class='output'>>></span> " + message + "</p>");
    window.scrollTop(window.prop("scrollHeight"));
 }
 
 function clearMessages(p, window = $('#messages')) {
    window.html("");
+}
+
+function printLines(p) {
+   appendMessage(p, "Lines of code: <strong>" + p.points.toLocaleString() + "</strong>");
+}
+
+function updateMoney(p) {
+   $('.money').html("$" + p.money.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
 }
